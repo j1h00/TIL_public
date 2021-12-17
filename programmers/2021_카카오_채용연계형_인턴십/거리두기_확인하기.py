53c70f187941b7a275059f783d3161358c06bdca
j1h00 <guruboy@snu.ac.kr>
2021-12-17 23:28:06 +0900

    2021-12-17

--- programmers/2021_카카오_채용연계형_인턴십/거리두기_확인하기.py
+++ programmers/2021_카카오_채용연계형_인턴십/거리두기_확인하기.py
@@ -10,40 +10,57 @@
 
 places = [["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"], ["POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"], ["PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"], ["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"], ["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"]]
 
-N = 5
-adj = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-jump = [(i, j) for i in range(N) for j in range(N) if i + j == 2]
-flag = False
-for place in places:
-    for x in range(N):
-        for y in range(N):
-            if place[x][y] == 'P':
-                # 가능한 범위를 탐색한다. 
-                for dx, dy in adj:
-                    nx, ny = x + dx, y + dy 
-                    if 0 <= nx < N and 0 <= ny < N and place[nx][ny] == 'P':
-                        flag = True
-                        break 
-                
-                for dx, dy in jump:
-                    nx, ny = x + dx, y + dy
-                    if 0 <= nx < N and 0 <= ny < N and place[nx][ny] == 'P':
-                        # partition 이 있는지 확인한다 
-                        if x == nx or y == ny: # 일직선 상에 있는 것이므로 
-                            if place[(nx + x)//2][(ny + y)//2] != 'X':
-                                flag = True 
-                                break 
-                        else: # 대각선인 경우, 몇개로 나눌까? 4개? 2개?
-                            if x < nx and y < ny:
-                                if place[x+1][y] != 'X' or place[x][y+1] != 'X':
+def solution(places):
+    answer = []
+    N = 5
+    adj = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    jump = [(i, j) for i in range(-2, 3) for j in range(-2, 3) if abs(i) + abs(j) == 2]
+    for place in places:
+        flag = False
+        for x in range(N):
+            for y in range(N):
+                if place[x][y] == 'P':
+                    # 가능한 범위를 탐색한다. 
+                    for dx, dy in adj: # 거리 1인 경우 
+                        nx, ny = x + dx, y + dy 
+                        if 0 <= nx < N and 0 <= ny < N and place[nx][ny] == 'P':
+                            flag = True
+                            break 
+                    
+                    for dx, dy in jump: # 거리 2인 경우 
+                        nx, ny = x + dx, y + dy
+                        if 0 <= nx < N and 0 <= ny < N and place[nx][ny] == 'P':
+                            # partition 이 있는지 확인한다 
+                            if x == nx or y == ny: # 일직선 상에 있는 경우
+                                if place[(nx + x)//2][(ny + y)//2] != 'X':
                                     flag = True 
                                     break 
-                            if x < nx and y < ny:
-                                if place[x+1][y] != 'X' or place[x][y+1] != 'X':
-                                    flag = True 
-                                    break
-                                
-                                
+                            else: # 대각선인 경우, 몇개로 나눌까? 4개? 2개?
+                                if x < nx and y < ny:
+                                    if place[x+1][y] != 'X' or place[x][y+1] != 'X':
+                                        flag = True 
+                                        break 
+                                elif x > nx and y > ny:
+                                    if place[x-1][y] != 'X' or place[x][y-1] != 'X':
+                                        flag = True 
+                                        break
+                                elif x > nx and y < ny:
+                                    if place[x-1][y] != 'X' or place[x][y+1] != 'X':
+                                        flag = True 
+                                        break
+                                elif x < nx and y > ny:
+                                    if place[x+1][y] != 'X' or place[x][y-1] != 'X':
+                                        flag = True 
+                                        break
+                                    
+            if flag:
+                break 
+        
         if flag:
-            break 
-    
+            answer.append(0)
+        else:
+            answer.append(1)
+
+    return answer
+
+print(solution(places))
