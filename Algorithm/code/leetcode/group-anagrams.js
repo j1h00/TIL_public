94af36854af210c274ad1de75cb63b3e0ec8c8de
j1h00 <guruboy@snu.ac.kr>
2022-07-05 22:36:36 +0900

    leetcode: Add javascript solutions

---
+++ Algorithm/code/leetcode/group-anagrams.js
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/group-anagrams/
+
+var groupAnagrams = function (strs) {
+  let obj = {};
+  for (let str of strs) {
+    let letters = str.split("").sort().join("");
+    obj[letters] ? obj[letters].push(str) : (obj[letters] = [str]);
+  }
+  return Object.values(obj);
+};
